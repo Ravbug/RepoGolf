d575934ef5a10369aa432eb24840af6a109bba16
Ravbug <ravbuganimations@gmail.com>
2022-11-21 14:43:48 -0500

    Make it more data-driven

--- presets.js
+++ presets.js
@@ -1,11 +1,38 @@
 const presets = {
-    "engines":["RavEngine","godot","UnrealEngine","Lumberyard","Babylon.js","BabylonNative","Cocos2d-x","o3de"],
-    "runtimes": ["OpenJDK","DotNET-Runtime","Chromium-V8","NodeJS"],
-    "frameworks": ["boost","Qt 6","OpenJDK","DotNET-Runtime"],
-    "gui" : ["Qt 6","Maui","wxWidgets","slint","gtk","Electron","JUCE","React"],
-    "languages": ["rust","Swift","cpython","kotlin","zig","fsharp","ruby","golang","PHP","Dart"],
-    "databases": ["MongoDB","FoundationDB","MySQL","PostgreSQL","sqlite"],
-    "browsers": ["Chromium-All","WebKit","Firefox"],
-    "apps" : ["Chromium-All","Firefox","Blender","UnrealEngine","godot","Visual Studio Code","Musescore","LMMS"],
-    "os": ["linux","SerenityOS","XNU","FreeBSD","ChromeOS","ArduPilot","PixHawk4","NuttX"],
+    "engines": {
+        label: "Game Engines",
+        items: ["RavEngine","godot","UnrealEngine","Lumberyard","Babylon.js","BabylonNative","Cocos2d-x","o3de"],
+    },
+    "runtimes":{
+        label: "Runtimes",
+        items: ["OpenJDK","DotNET-Runtime","Chromium-V8","NodeJS"],
+    },
+    "frameworks": {
+        label: "Frameworks",
+        items: ["boost","Qt 6","OpenJDK","DotNET-Runtime"],
+    },
+    "gui" : { 
+        label: "GUI",
+        items: ["Qt 6","Maui","wxWidgets","gtk","Electron","JUCE","React"]
+    },
+    "languages" : {
+        label: "Programming Languages",
+        items: ["rust","Swift","cpython","kotlin","zig","fsharp","ruby","golang","PHP","Dart"]
+    },
+    "databases":{ 
+        label: "Databases",
+        items: ["MongoDB","FoundationDB","MySQL","PostgreSQL","sqlite"]
+    },
+    "browsers": {
+        label: "Browsers",
+        items: ["Chromium-All","WebKit","Firefox"],
+    },
+    "apps" : {
+        label: "Apps",
+        items: ["Chromium-All","Firefox","Blender","UnrealEngine","godot","Visual Studio Code","MuseScore","LMMS"]
+    },
+    "os": {
+        label: "Operating Systems and Firmwares",
+        items: ["linux","SerenityOS","XNU","FreeBSD","ChromeOS","ArduPilot","PixHawk4","NuttX"]
+    } ,
 }
